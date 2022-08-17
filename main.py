a2122cfecd63ad85c143d4c0d8e56ec9ca291224
505 <505@academicos.cesde.edu.co>
2022-08-17 17:37:22 -0500

    se crean datos primitivos

---
+++ main.py
@@ -0,0 +1,20 @@
+#Hola me llamo comentario de linea
+
+'''
+
+prrprprprp
+hola soy un comentario de bloque.
+
+'''
+
+#datos==entrada==variables
+
+#datos primitivos
+nombre="Juanes"
+edad= 32
+estatura= 1.73
+Hinchadelmejor= True
+
+#salidas
+print("hola:  ",nombre , "tu estatura es", estatura)
+print(f'Hola {nombre} tu estatura es {estatura} ')
